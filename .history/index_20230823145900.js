9ae89e747711512f16dae5395347177103c07cae
Newton Mboi <newtieblackie@gmail.com>
2023-08-23 15:06:35 +0300

    Missing number finder

---
+++ .history/index_20230823145900.js
@@ -0,0 +1,19 @@
+function findMissingNumber(arr) {
+    const n = 45; // Total number of integers
+    const totalSum = (n * (n + 1)) / 2; // Sum of the first n integers
+    const arraySum = arr.reduce((acc, num) => acc + num, 0); // Sum of elements in the array
+  
+    return totalSum - arraySum;
+  }
+  
+  const numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+    42, 43, 44, 45,
+  ]; // Array of the first 45 numbers with one missing
+  const missingNumber = findMissingNumber(numbers);
+  
+  document.getElementById("numbers").textContent = JSON.stringify(numbers);
+  document.getElementById("missingNumber").textContent = missingNumber;
+  
+//   console.log("Missing number:", missingNumber);
